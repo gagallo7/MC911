13945c76437d5ac3289bfd867f0831d3fd88263f
ursopanda <gagallo7@gmail.com>
2014-04-16 16:43:30 -0300

    getMethodList and getStatementList added

--- Project2/pacote/src/llvm/LlvmUtility.java
+++ Project2/pacote/src/llvm/LlvmUtility.java
@@ -3,10 +3,21 @@ import syntaxtree.*;
 
 public class LlvmUtility
 {
-    public static final java.util.List < VarDecl > getList ( util.List < VarDecl > arg )
+    public static final java.util.List < VarDecl > getVarList ( util.List < VarDecl > arg )
     {
         util.List < VarDecl > aux = arg;
         java.util.List < VarDecl > res = new java.util.LinkedList < VarDecl > ();
+        while ( aux != null )
+        {
+            res.add ( aux.head );
+            aux = aux.tail;
+        }
+        return res;
+    }
+    public static final java.util.List < MethodDecl > getMethodList ( util.List < MethodDecl > arg )
+    {
+        util.List < MethodDecl > aux = arg;
+        java.util.List < MethodDecl > res = new java.util.LinkedList < MethodDecl > ();
         while ( aux != null )
         {
             res.add ( aux.head );
