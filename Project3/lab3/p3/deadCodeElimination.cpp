36f48c99784b1e4a22ade6017ef6d14fdf3678df
Bruno Gsa <brunogsa.92@gmail.com>
2014-06-11 14:01:39 -0300

    GG?

--- Project3/lab3/p3/deadCodeElimination.cpp
+++ Project3/lab3/p3/deadCodeElimination.cpp
@@ -258,58 +258,61 @@ namespace
             //         compute all Instructions use/def
             // ===========================================
 
-/*
- *            for( Function::iterator i = func->begin(); i != func->end(); i++ ) 
- *            {
- *                // For every Instruction inside a BasicBlock...
- *                for ( BasicBlock::iterator j = i->begin(); j != i->end(); j++ ) 
- *                {
- *                    unsigned int n = j->getNumOperands();
- *
- *                    for( unsigned int k = 0; k < n; k++ ) 
- *                    {
- *                        Value* v = j->getOperand(k);
- *
- *                        if( isa<Instruction>(v) ) 
- *                        {
- *                            Instruction *op = cast<Instruction>(v);
- *
- *                            if ( !data.instructions[j]->use.count(op) ) 
- *                                data.instructions[j]->use.insert(op);
- *                        }
- *                    }
- *
- *                    data.instructions[j]->def.insert( j );
- *                }
- *            }
- */
+            for( Function::iterator i = func->begin(); i != func->end(); i++ ) 
+            {
+                // For every Instruction inside a BasicBlock...
+                for ( BasicBlock::iterator j = i->begin(); j != i->end(); j++ ) 
+                {
+                    unsigned int n = j->getNumOperands();
+
+                    for( unsigned int k = 0; k < n; k++ ) 
+                    {
+                        Value* v = j->getOperand(k);
+
+                        if( isa<Instruction>(v) ) 
+                        {
+                            Instruction *op = cast<Instruction>(v);
+
+                            if ( !data.instructions[j]->use.count(op) ) 
+                                data.instructions[j]->use.insert(op);
+                        }
+                    }
+
+                    data.instructions[j]->def.insert( j );
+                }
+            }
+
+            errs() << ":D:D:D:D Step 4\n\n";
 
             // ===========================================
             // Step 4: Use data from BasicBLocks to
             //         compute all Instructions in/out
             // ===========================================
 
-/*
- *            for( Function::iterator i = func->begin(); i != func->end(); i++ ) 
- *            {
- *                // Last instruction of the block
- *                BasicBlock::iterator j = i->end();
- *                j--;
- *                data.instructions[j]->out = data.blocks[i]->out;
- *                data.instructions[j]->in = getSetUnion( data.instructions[j]->use, getSetDifference( data.instructions[j]->out, data.instructions[j]->def ) );
- *
- *                // Other instructions
- *                do 
- *                {
- *                    BasicBlock::iterator aux = j;
- *                    j--;
- *
- *                    data.instructions[j]->out = data.instructions[aux]->in;
- *                    data.instructions[j]->in = getSetUnion( data.instructions[j]->use, getSetDifference( data.instructions[j]->out, data.instructions[j]->def ) );
- *
- *                } while( j != i->begin() ); 
- *            }
- */
+            for( Function::iterator i = func->begin(); i != func->end(); i++ ) 
+            {
+                // Last instruction of the block
+                BasicBlock::iterator j = i->end();
+                j--;
+                data.instructions[j]->out = data.blocks[i]->out;
+                data.instructions[j]->in = getSetUnion( data.instructions[j]->use, getSetDifference( data.instructions[j]->out, data.instructions[j]->def ) );
+
+                // Other instructions
+                BasicBlock::iterator aux = j;
+
+                while( j != i->begin() )
+                {
+                    aux = j;
+                    j--;
+
+                    data.instructions[j]->out = data.instructions[aux]->in;
+                    data.instructions[j]->in = getSetUnion( data.instructions[j]->use, getSetDifference( data.instructions[j]->out, data.instructions[j]->def ) );
+
+                } 
+
+                data.instructions[j]->out = data.instructions[aux]->in;
+                data.instructions[j]->in = getSetUnion( data.instructions[j]->use, getSetDifference( data.instructions[j]->out, data.instructions[j]->def ) );
+            }
 
             // ===========================================
             // Returning
